e3dbc1004cd2eb6dccdbd58cb2ac7a4c3cd71a15
haoran0115 <sunhaorang160522@gmail.com>
2022-09-21 15:21:06 +0800

    update Assignment 2

--- Assignment 2/plot.py
+++ Assignment 2/plot.py
@@ -1,9 +1,10 @@
 #!/usr/bin/bash
+from cProfile import label
 import matplotlib.pyplot as plt
 import seaborn
 import numpy as np
 seaborn.set_style("whitegrid")
-plt.rcParams.update({'font.size': 14})
+plt.rcParams.update({'font.size': 16})
 
 fig, ax = plt.subplots(figsize=[8,6])
 ax.set_ylim([-0.1, 1.3])
@@ -41,6 +42,14 @@ ax.plot(np.array(x2)[:, 0], np.array(x2)[:, 1], label=r"$t=a/c$")
 ax.plot(np.array(x3)[:, 0], np.array(x3)[:, 1], label=r"$t=3a/2c$")
 ax.plot(np.array(x4)[:, 0], np.array(x4)[:, 1], label=r"$t=2a/c$")
 ax.plot(np.array(x5)[:, 0], np.array(x5)[:, 1], label=r"$t=5a/c$")
+ax.set_xlabel(r"$x$")
+ax.set_ylabel(r"$u$")
+
+fig.canvas.draw()
+
+ylabels = [w.get_text()+r" $a/c$" for w in ax.get_yticklabels()]
+print(ylabels)
+ylabels = ax.set_yticklabels(ylabels)
 
 plt.legend(loc='upper right')
 plt.tight_layout()
